961f1d54d0efa9843d6b05a99160c82658dc43fd
Jan Trejbal <jan.trejbal@scalesoft.cz>
2019-07-29 17:03:26 +0200

    Add publish script

---
+++ publish.ps1
@@ -0,0 +1,49 @@
+#!/usr/bin/env pwsh
+
+    [CmdletBinding()]
+Param(
+    [Parameter(Mandatory = $true)]
+    [String]$apiKey,
+
+    [Parameter(Mandatory = $true)]
+    [String]$packageVersion,
+
+    [Parameter()]
+    [Switch]$disableRestore = $false
+)
+
+$currentPath = (Get-Location -PSProvider FileSystem).ProviderPath
+
+$packages = New-Object System.Collections.ArrayList
+$packages.Add("DryIoc.Facilities.AutoTx") > $null
+$packages.Add("DryIoc.Facilities.NHibernate") > $null
+$packages.Add("DryIoc.Transactions") > $null
+
+if ($packageVersion[0] -eq 'v')
+{
+    $packageVersion = $packageVersion.Substring(1)
+}
+
+if (!$disableRestore)
+{
+    dotnet restore
+}
+
+# `dotnet build` must run before `dotnet publish` because GeneratePackageOnBuild in csproj forces not to build when running `dotnet publish` command
+# https://github.com/dotnet/core/issues/1778
+dotnet build --no-restore -c Release "/property:Version=${packageVersion}"
+
+dotnet publish --no-restore -c Release --no-build
+
+Set-Location $currentPath
+Set-Location build
+
+foreach ($package in $packages)
+{
+	$packageFullName = "${package}.${packageVersion}.nupkg"
+
+    Write-Host "Publishing ${packageFullName}"
+    dotnet nuget push -k "${apiKey}" "${packageFullName}" --source "http://proget.scalesoft.cz/nuget/Nuget/"
+}
+
+Set-Location $currentPath
